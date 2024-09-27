5eded57ad073ec5b5f6cfb2877c76c106019d8a3
Shant Dashjian <shantgdashjian@gmail.com>
2024-09-27 19:32:34 +0400

    Add arrays example

---
+++ chapter4/labs/Arrays.java
@@ -0,0 +1,11 @@
+package chapter4.labs;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+	    int[] numbers = {1, 2, 3};
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println(i);
+		}
+	}
+}
